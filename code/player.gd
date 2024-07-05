5a22f9706678412ecda9159fefdc044135915257
troingle <109359272+troingle@users.noreply.github.com>
2024-07-05 09:28:13 +0000

    Added existing files

---
+++ code/player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+var speed = 220
+var accel = 100
+
+var dir
+
+@onready var sprite = $AnimatedSprite2D
+@onready var anim = $AnimationPlayer
+
+var stickObj = preload("res://scenes/stick.tscn")
+
+func _physics_process(delta):
+	dir = Input.get_vector("left", "right", "up", "down")
+	velocity.x = move_toward(velocity.x, speed * dir.x, accel)
+	velocity.y = move_toward(velocity.y, speed * dir.y, accel)
+
+	if dir.y > 0:
+		sprite.play("run_fwd")
+		anim.play("wobble")
+	elif dir.y < 0:
+		sprite.play("run_bwd")
+		anim.play("wobble")
+	elif dir.x:
+		sprite.play("run_side")
+		anim.play("wobble")
+	else:
+		sprite.play("idle")
+		anim.play("RESET")
+		
+	if dir.x > 0:
+		sprite.flip_h = true
+	elif dir.x < 0:
+		sprite.flip_h = false
+
+	if visible:
+		move_and_slide()
+	
+	if Input.is_action_pressed("exit"):
+		get_tree().quit()
+		
+	if Input.is_action_just_pressed("place"):
+		var stick = stickObj.instantiate()
+		$"..".add_child(stick)
+		stick.position = position
