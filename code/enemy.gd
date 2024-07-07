f71c5ea4b98d8792b87b9b3b2744944a7b4c97cc
troingle <bugsinsidemyhead@gmail.com>
2024-07-07 17:48:41 +0000

    Added level 5 and new camera enemy

--- code/enemy.gd
+++ code/enemy.gd
@@ -13,8 +13,10 @@ var dir = 1
 @onready var anim = $AnimationPlayer
 
 @export var vertical = false
+@export var spinner = false
 @export var length = 400
 @export var speed = 120
+@export var spin_dir = 1
 
 var rng = RandomNumberGenerator.new()
 
@@ -28,26 +30,30 @@ func _ready():
 	sprite.play("walk" + str(rng.randi_range(1, 6)))
 
 func _physics_process(delta):
-	if vertical:
-		velocity.y = speed * dir
-		if position.y >= target.y:
-			dir = -1
-		elif position.y <= start.y:
-			dir = 1
-		if dir == 1:
-			view.rotation = deg_to_rad(90)
+	if not spinner:
+		if vertical:
+			velocity.y = speed * dir
+			if position.y >= target.y:
+				dir = -1
+			elif position.y <= start.y:
+				dir = 1
+			if dir == 1:
+				view.rotation = deg_to_rad(90)
+			else:
+				view.rotation = deg_to_rad(-90)
 		else:
-			view.rotation = deg_to_rad(-90)
+			velocity.x = speed * dir
+			if position.x >= target.x:
+				dir = -1
+			elif position.x <= start.x:
+				dir = 1
+			if dir == 1:
+				view.rotation = 0
+			else:
+				view.rotation = deg_to_rad(-180)
 	else:
-		velocity.x = speed * dir
-		if position.x >= target.x:
-			dir = -1
-		elif position.x <= start.x:
-			dir = 1
-		if dir == 1:
-			view.rotation = 0
-		else:
-			view.rotation = deg_to_rad(-180)
+		view.rotation += 0.024 * spin_dir
+		sprite.play("camera")
 			
 	if not Global.paused:
 		move_and_slide()
